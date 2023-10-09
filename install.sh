e3a7a6fe89f9aa4675e74aa2745c399b12b1b0a8
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-09 13:39:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,26 @@
 clear
-sudo pacman -S intellij-idea
+echo "updating system"
+sudo pacman -Syu
+sudo pacman -Syuu
+
+echo "installing java"
+sudo pacman -S jre-openjdk
+sudo pacman -S jdk-openjdk
+
+echo "installing node js"
+sudo pacman -S nodejs npm
+
+echo "installing snap"
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+echo "installing intellij idea"
+sudo snap install intellij-idea-community --classic
+
+echo "installing pycharm"
+sudo snap install pycharm-community --classic
+
+echo "installing vscode"
+sudo pacman -S code
+
