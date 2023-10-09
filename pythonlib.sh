d2a448208205e1b255647f0cfdaf31b36c804c53
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-09 13:52:03 +0300

    Create pythonlib.sh

---
+++ pythonlib.sh
@@ -0,0 +1,12 @@
+clear
+echo "data science"
+pip install numpy pandas scikit-learn matplotlib seaborn
+
+echo "web development"
+pip install flask django fastapi
+
+echo "natural language processing"
+pip install nltk textblob
+
+echo "graphical"
+pip install kivy
