571b45fa61e9c9c1d33df22b487863e75abe241e
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-09 14:05:07 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,22 @@
 clear
+echo "updating and upgrading the system"
 sudo apt update 
 sudo apt upgrade
+
+echo "installing snap"
+sudo apt install snapd
+
+echo "installing code"
+sudo snap install --classic code
+
+echo "installing java"
+sudo apt install default-jre
+
+echo "installing ant"
+sudo apt install ant
+
+echo "installing intellij idea"
+sudo snap install intellij-idea-community --classic
+
+echo "installing pycharm"
+sudo snap install pycharm-community --classic
