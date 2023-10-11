8d3657c94cff915cfab1c46d7983530d9c765108
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-11 12:40:28 +0300

    added rust and ruby

--- ubuntu.sh
+++ ubuntu.sh
@@ -15,6 +15,15 @@ sudo apt install default-jre
 echo "installing ant"
 sudo apt install ant
 
+echo "installing ruby"
+sudo apt-get install ruby
+
+echo "installing rust"
+curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+sudo apt upgrade && sudo apt update
+sudo apt install build-essentials
+
 echo "installing intellij idea"
 sudo snap install intellij-idea-community --classic
 
