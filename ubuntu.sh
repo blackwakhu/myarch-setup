5e95191d756761c68e7d37e04aeae731d08fc579
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-11 13:16:21 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,6 +3,10 @@ echo "updating and upgrading the system"
 sudo apt update 
 sudo apt upgrade
 
+echo "installng wget and curl"
+sudo apt install wget
+sudo apt install curl
+
 echo "installing snap"
 sudo apt install snapd
 
@@ -32,8 +36,38 @@ sudo apt install mongodb-org
 sudo systemctl start mongod.service
 sudo systemctl enable mongod
 
+echo "installing mariadb"
+sudo apt update
+sudo apt install mariadb-server
+sudo systemctl start mariadb.service
+sudo mysql_secure_installation
+
+echo "installing mono"
+sudo apt install gnupg ca-certificates
+sudo apt install apt-transport-https dirmngr gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-buster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-devel
+sudo apt-get install mono-develop
+
+echo "installing dot net"
+wget https://packages.microsoft.com/config/ubuntu/23.04/packages-microsoft-prod.deb
+
+echo "installing snapd"
+sudo apt install snapd
+
 echo "installing intellij idea"
 sudo snap install intellij-idea-community --classic
 
 echo "installing pycharm"
 sudo snap install pycharm-community --classic
+
+echo "installing dbeaver"
+sudo snap install dbeaver-ce
+
+echo "installing vlc"
+sudo snap install vlc
+
+echo "installing gimp"
+sudo snap install gimp
