49305493063775ee1fd315997a8d77a50db1b409
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-10-11 12:49:18 +0300

    added mongodb

--- ubuntu.sh
+++ ubuntu.sh
@@ -24,6 +24,14 @@ source $HOME/.cargo/env
 sudo apt upgrade && sudo apt update
 sudo apt install build-essentials
 
+echo "installing mongodb"
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl enable mongod
+
 echo "installing intellij idea"
 sudo snap install intellij-idea-community --classic
 
