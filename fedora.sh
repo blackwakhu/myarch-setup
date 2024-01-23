73ccab7bf86db5ec99af19a691a4f668c8a22a44
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-23 15:22:18 +0300

    added pycharm

--- fedora.sh
+++ fedora.sh
@@ -1,4 +1,8 @@
+sudo dnf upgrade --refresh
 sudo dnf update
 sudo dnf install community-mysql-server -y
 
 sudo dnf -y install gimp
+
+sudo dnf copr enable phracek/PyCharm -y
+sudo dnf install pycharm-community
