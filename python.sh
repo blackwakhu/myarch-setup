d0e811253f380e20d025ff98915757305e5b75d2
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-01-23 15:01:42 +0300

    added some features to the python file that deal with flask

--- python.sh
+++ python.sh
@@ -1,9 +1,15 @@
 clear
 echo "data science"
-pip install numpy pandas scikit-learn matplotlib seaborn
+pip install numpy pandas scikit-learn matplotlib seaborn plotly
 
 echo "web development"
 pip install flask django fastapi uvicorn
+
+echo "some flask related things"
+pip install flask_restful sqlalchemy flask_sqlalchemy flask_wtf flask_pymongo flask_marshmallow 
+
+echo "dealing with databases"
+pip install pymongo pymysql
 
 echo "natural language processing"
 pip install nltk textblob
@@ -13,3 +19,6 @@ pip install kivy
 
 echo "web scrapping"
 pip install requests beautifulsoup4 Scrapy
+
+echo "installing some game things"
+pip install pygame
